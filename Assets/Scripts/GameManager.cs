1b153255a8fb464a4508d39379f0fd030f59530a
Nyeste Miki <nyeste.miki@yahoo.com>
2020-02-29 03:06:19 +0200

    First commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+    [SerializeField]
+    private GameObject platePrefab = null;
+    [SerializeField]
+    private Canvas ui = null;
+    
+    private GameObject currentPlate = null;
+
+    private float height = 2.0f;
+
+    private bool gameOver = false;
+
+    void Awake()
+    {
+        NewPlate();
+    }
+
+    void Update()
+    {
+        if (gameOver)
+        {
+            return;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            DropCurrentPlate();
+            NewPlate();
+        }
+    }
+
+    private void DropCurrentPlate()
+    {
+        currentPlate.GetComponent<Plate>().enabled = false;
+        currentPlate.GetComponent<Rigidbody>().useGravity = true;
+        currentPlate.GetComponent<Rigidbody>().isKinematic = false;
+    }
+
+    private void NewPlate()
+    {
+        Vector3 position = new Vector3(0, height, 0);
+        GameObject newPlate = Instantiate(platePrefab, position, Quaternion.identity);
+        height += .5f;
+        currentPlate = newPlate;
+        transform.position += new Vector3(0, .3f, 0);
+    }
+
+    public void PlateHitGround()
+    {
+        gameOver = true;
+        DropCurrentPlate();
+        ui.GetComponent<Canvas>().enabled = true;
+    }
+}
---
+++ Assets/Scripts/Plate.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class Plate : MonoBehaviour
+{
+    [SerializeField]
+    private float leftBorder = 0.0f;
+    [SerializeField]
+    private float rightBorder = 0.0f;
+    [SerializeField]
+    private float speed = 0.0f;
+
+    // True: left => right
+    private bool direction = true;
+    
+    void Update()
+    {
+        if (direction)
+        {
+            transform.Translate(Vector3.right * (speed * Time.deltaTime));
+        } else
+        {
+            transform.Translate(Vector3.left * (speed * Time.deltaTime));
+        } 
+
+        if (transform.position.x > rightBorder)
+        {
+            direction = false;
+        }
+        if (transform.position.x < leftBorder)
+        {
+            direction = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Ground"))
+        {
+            GameManager.Instance.PlateHitGround();
+        }
+    }
+}
